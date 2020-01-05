cf7d9484192f67a39323da2cde43fed2abaee1a3
larshp <larshp@hotmail.com>
2020-01-05 15:25:26 +0100

    add diagram

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # cloud_foundry
 testing
+
+![Components](http://www.plantuml.com/plantuml/proxy?cache=no&src=https://raw.githubusercontent.com/abaplint/abaplint-cloud-foundry/master/components.iuml)
--- src/server.ts
+++ src/server.ts
@@ -14,6 +14,10 @@ app.post("/api/v1/check_file", function (req, res) {
   res.send(JSON.stringify(checkObject(req.body), null, 2));
 });
 
+// app.post("/api/v1/check_configuration",
+// app.post("/api/v1/default_configuration",
+// app.post("/api/v1/pretty_print",
+
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log("listening on port " + port);
