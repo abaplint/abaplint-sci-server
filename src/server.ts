91ddab34a7233bbef64581f4b783e303bf14a5e3
larshp <larshp@hotmail.com>
2020-01-05 18:56:46 +0100

    output some stats

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import * as express from "express";
 import * as abaplint from "abaplint";
+import * as os from "os";
 import { checkObject } from "./check_object";
 
 const app = express();
@@ -7,7 +8,16 @@ app.use(express.json());
 app.use (express.urlencoded({extended: false}));
 
 app.get("/", function (_req, res) {
-  res.send("abaplint: " + abaplint.Registry.abaplintVersion());
+
+  const response = "abaplint: " + abaplint.Registry.abaplintVersion() + "<br>" +
+    "<hr>" +
+    "load: " + os.loadavg() + "<br>" +
+    "uptime: " + os.uptime() + "<br>" +
+    "freemem: " + os.freemem() + "<br>" +
+    "hostname: " + os.hostname() + "<br>" +
+    "platform: " + os.platform() + "<br>" +
+    "cpus: " + JSON.stringify(os.cpus()) + "<br>";
+  res.send(response);
 });
 
 app.post("/api/v1/check_file", function (req, res) {
@@ -17,6 +27,7 @@ app.post("/api/v1/check_file", function (req, res) {
 // app.post("/api/v1/check_configuration",
 // app.post("/api/v1/default_configuration",
 // app.post("/api/v1/pretty_print",
+// app.post("/api/v1/ping",
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
