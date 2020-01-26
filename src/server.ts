5d39c58d7a361217d4c6ea506b27a19a6124a5c0
larshp <larshp@hotmail.com>
2020-01-26 14:49:55 +0100

    update deps

--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,8 @@ import {frontPage} from "./front_page";
 const info: string[] = [];
 
 const app = express();
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb', extended: false}));
+app.use(express.json({limit: "50mb"}));
+app.use(express.urlencoded({limit: "50mb", extended: false}));
 
 app.get("/", function (_req, res) {
   res.send(frontPage(info));
