fd6e5b5dfa52f06a1a5481c503ae90d66c1ef162
Alexander Tsybulsky <atsybulsky@sbcg.com.ua>
2020-03-24 00:08:06 +0200

    tests

---
+++ src/app.smoke.test.ts
@@ -0,0 +1,14 @@
+import app from "./app";
+import * as request from "supertest";
+
+test("smoke test: ping", async () => {
+  const res = await request(app).post("/api/v1/ping");
+  expect(res.status).toBe(200);
+  expect(res.body).toEqual({ success: true, payload: "abap is forevah!" });
+});
+
+test("smoke test: 404", async () => {
+  const res = await request(app).get("/api/v1/zzz");
+  expect(res.status).toBe(404);
+  expect(res.body).toEqual({ success: false, error:{ message: "Wrong API call" }});
+});
